93731c8683fd02559151b30e7fe7b4329b814c82
apdlwjwjwj <apdlwjwjwj@gmail.com>
2025-11-01 16:42:51 +0900

    feat(constant): 상수추가
    출력관련 문자열을 Message, Error 객체로 관리

---
+++ src/main/kotlin/lotto/constant/Error.kt
@@ -0,0 +1,11 @@
+package lotto.constant
+
+object Error {
+    const val ERROR_PREFIX = "[ERROR]"
+
+    const val INVALID_NUMBER = "숫자가 아닙니다."
+    const val INVALID_PURCHASE_UNIT = "1000단위가 아닙니다."
+    const val INVALID_SIZE = "번호 개수가 6개가 아닙니다."
+    const val DUPLICATE_NUMBER = "번호가 중복되었습니다."
+    const val INVALID_RATE = "1~45사이의 숫자가 아닙니다."
+}
---
+++ src/main/kotlin/lotto/constant/Message.kt
@@ -0,0 +1,11 @@
+package lotto.constant
+
+object Message {
+    const val REQUEST_AMOUNT = "구입금액을 입력해 주세요."
+    const val COUNT = "%d개를 구매했습니다."
+    const val REQUEST_NUMBER = "당첨 번호를 입력해 주세요."
+    const val REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val WINNING_STATUS = "당첨 통계\n---"
+    const val STATUS = "%d개 일치 (%,d원) - %d개"
+    const val PROFIT = "총 수익률은 %1.f%%입니다."
+}
